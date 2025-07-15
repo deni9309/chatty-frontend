f3cf37f8e55ba5d950a610f48bab8c01385b574d
deni9309 <deni9309@gmail.com>
2025-07-16 00:18:32 +0300

    Commit progress on chat container component

--- src/components/chat/chat.container.tsx
+++ src/components/chat/chat.container.tsx
@@ -5,9 +5,12 @@ import ChatHeaderContainer from './chat-header.container'
 import MessageInput from '../shared/message-input'
 import MessageSkeleton from '../skeletons/message-skeleton'
 import { cn } from '../../lib/utils/clsx'
+import { useAuthStore } from '../../store/use-auth.store'
+import { formatTimestamp } from '../../lib/utils/date-format.util'
 
 const ChatContainer = () => {
   const { selectedUser, messages, areMessagesLoading, getMessages } = useChatStore()
+  const { authUser, isCheckingAuth } = useAuthStore()
 
   useEffect(() => {
     if (selectedUser) {
@@ -15,7 +18,7 @@ const ChatContainer = () => {
     }
   }, [selectedUser, getMessages])
 
-  if (areMessagesLoading) {
+  if (areMessagesLoading || isCheckingAuth || !selectedUser) {
     return (
       <div className="flex flex-1 overflow-y-auto flex-col">
         <ChatHeaderContainer />
@@ -32,21 +35,50 @@ const ChatContainer = () => {
       <div
         className={cn(
           'flex-1 overflow-y-auto h-full p-2 space-y-2',
-          window.innerWidth < 900 ? 'max-h-[calc(100dvh-280px)]' : 'max-h-[calc(100dvh-220px)]',
+          window.innerWidth < 900 ? 'max-h-[calc(100dvh-270px)]' : 'max-h-[calc(100dvh-220px)]',
         )}
       >
         {messages.length === 0 ? (
           <NoChatMessagesContainer selectedUser={selectedUser} />
         ) : (
           messages.map((m) => (
-            <div key={m._id}>
-              {m.text !== '' && <p className="chat-bubble">{m.text}</p>}
-              {m.image !== '' && <img className="chat-image" src={m.image} />}
+            <div
+              key={m._id}
+              className={cn('chat', m.senderId === authUser?._id ? 'chat-end' : 'chat-start')}
+            >
+              <div className="chat-image avatar">
+                <div className="size-10 bg-base-300 rounded-full border">
+                  <img
+                    src={
+                      m.senderId === authUser?._id
+                        ? authUser.profilePic || '/user.svg'
+                        : selectedUser?.profilePic || '/user.svg'
+                    }
+                    className="size-full p-0.5 object-cover"
+                    alt="Profile Image"
+                  />
+                </div>
+              </div>
+              <div className="chat-header mb-1">
+                <time className="text-xs opacity-50">{formatTimestamp(m.createdAt)}</time>
+              </div>
+              <div className="chat-bubble">
+                {m.text !== '' && <p className="mb-1">{m.text}</p>}
+                {m.image !== '' && (
+                  <img
+                    className="lg:chat-image w-full rounded sm:max-w-sm max-sm:max-w-xs"
+                    src={m.image}
+                    alt="Attached Image"
+                  />
+                )}
+              </div>
             </div>
           ))
         )}
       </div>
-      <div className="w-full absolute bottom-0">
+      <div
+        className={cn('w-full absolute', window.innerWidth < 900 ? 'bottom-[50px]' : 'bottom-0')}
+      >
         <MessageInput />
       </div>
     </div>
