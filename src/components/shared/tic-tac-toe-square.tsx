87a5653ac4596f335a9b1e6fba42a70a5678823a
deni9309 <deni9309@gmail.com>
2025-09-09 22:42:43 +0300

    Fix: Improve tic-tac-toe game component UI.

--- src/components/shared/tic-tac-toe-square.tsx
+++ src/components/shared/tic-tac-toe-square.tsx
@@ -12,7 +12,7 @@ const TicTacToeSquare = ({ value, onClick, disabled }: TicTacToeSquareProps) => 
     <button
       onClick={onClick}
       disabled={disabled}
-      className="f-center size-24 sm:size-28 border border-base-300 text-5xl font-bold transition-colors disabled:cursor-not-allowed"
+      className="f-center size-20 sm:size-24 border border-base-300 text-5xl font-bold transition-colors disabled:cursor-not-allowed"
     >
       <span className={cn(value === 'X' ? 'text-primary' : 'text-secondary')}>{value}</span>
     </button>
--- src/pages/games-page.tsx
+++ src/pages/games-page.tsx
@@ -1,8 +1,8 @@
 import TicTacToeGame from '../components/games/tic-tac-toe.game'
 
 const GamesPage = () => (
-  <div className="w-full h-full">
-    <h1>My Chatty Games</h1>
+  <div className="flex flex-col w-full h-full items-center mx-auto space-y-4">
+    <h1 className="text-2xl font-bold my-4 uppercase">Games</h1>
     <TicTacToeGame />
   </div>
 )
